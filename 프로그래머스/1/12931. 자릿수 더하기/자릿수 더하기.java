90b943e1e03e0fadb7da58576b9b724ff12f0599
CHONGMYOUNG LEE <53869165+dlchdaud123@users.noreply.github.com>
2024-11-05 11:35:47 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,14 +3,13 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        while(true){
-            answer += n %10;
-            n = n / 10;
-            
-            if(n < 10){
-                answer += n;
+        for(int i = 0; i < 100; i++){
+
+                answer += n % 10;
+            if(n<10){
                 break;
-            }
+            }   
+            n = n / 10;
         }
         return answer;
     }
