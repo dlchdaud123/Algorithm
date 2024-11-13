3c2517e008ec980e71d26fadf9d83157b16f2462
CHONGMYOUNG LEE <53869165+dlchdaud123@users.noreply.github.com>
2024-11-13 11:03:25 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -3,19 +3,17 @@ class Solution {
         int answer = 0;
         
         while(num != 1){
-            // 만약 작업 수가 500이라면 작업 수를 -1로 바꾸고 종료
             if(answer == 500){
-                answer = -1;
+            answer = -1;
                 break;
             }
             
-            // 입력된 수가 짝수 이면 2로 나눈다.
-            if(num % 2 == 0) num /= 2;
-            // 홀수면 3 곱하고 1 더한다.
+            if(num % 2 == 0){
+                num /= 2;
+            }
             else{
                 num = num * 3 + 1;
             }
-            // 작업 수
             answer++;
         }
         return answer;
