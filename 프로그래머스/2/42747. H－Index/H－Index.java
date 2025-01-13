eb65a56acc623be7b2bd3fe7d75559ca3b6e54f6
CHONGMYOUNG LEE <53869165+dlchdaud123@users.noreply.github.com>
2025-01-13 19:46:30 +0900

    [level 2] Title: H-Index, Time: 5.04 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,22 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] citations) {
-        // 배열을 내림차순 정렬
-        Integer[] arr = Arrays.stream(citations).boxed().toArray(Integer[]::new);
-        Arrays.sort(arr, Collections.reverseOrder());
-
-        int hIndex = 0;
-
-        // H-Index 계산
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] >= i + 1) {
-                hIndex = i + 1; // H-Index 후보
-            } else {
-                break; // 조건 불만족 시 종료
-            }
-        }
-
-        return hIndex;
+        int answer = 0;
+        return answer;
     }
 }
