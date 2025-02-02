9525326cd1a54d22ef4c090bd8e25ff6c468d58b
CHONGMYOUNG LEE <53869165+dlchdaud123@users.noreply.github.com>
2025-02-02 20:43:25 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 33.80 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,21 +1,28 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        List<Integer> resultList = new ArrayList<>();
+    public int[] solution(int[] arr) {
+        // 연속된 중복 값을 제거한 결과를 저장할 리스트 생성
+        List<Integer> resultList = new ArrayList<>(); 
         
-        resultList.add(arr[0]);
-        for(int i=0; i < arr.length; i++){
-            if(arr[i] != resultList.get(resultList.size() - 1)){
+        // 첫 번째 요소는 무조건 추가
+        resultList.add(arr[0]); 
+        
+        // 배열을 순회하며 연속된 중복 요소 제거
+        for (int i = 0; i < arr.length; i++) {
+            // 현재 요소가 resultList의 마지막 요소와 다르면 추가
+            if (arr[i] != resultList.get(resultList.size() - 1)) {
                 resultList.add(arr[i]);
             }
         }
         
+        // 리스트를 배열로 변환
         int[] answer = new int[resultList.size()];
-        for(int i = 0; i < resultList.size(); i++){
-            answer[i] = resultList.get(i);
+        for (int i = 0; i < resultList.size(); i++) {
+            // 리스트의 요소를 배열에 복사
+            answer[i] = resultList.get(i); 
         }
-
-        return answer;
+        // 중복이 제거된 배열 반환
+        return answer; 
     }
 }
